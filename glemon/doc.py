4a3a5f4de815bf4fe9af8185d34d4b0c4fc95093
huangtao-sh <huangtao.jh@gmail.com>
2018-06-04 08:40:57 +0800

    upgrade

--- glemon/doc.py
+++ glemon/doc.py
@@ -6,12 +6,25 @@
 # 创建：2018-05-30 20:22
 
 from .document import Document, P
-from orange import generator, split
+from orange import generator, split, Path, decode
 from orange.coroutine import wait
+from .loadcheck import LoadFile
+from aiofiles import open
+import xlrd
+
+
+class FileImported(Exception):
+
+    def __init__(self, filename):
+        super().__init__()
+        self.filename = filename
+
+    def __str__(self):
+        return '文件 %s 已导入数据库，跳过' % (self.filename)
 
 
 class NewDocument(Document):
-    load_kw = {}
+    loadfile_kw = {}
 
     @classmethod
     async def _insert(cls, rows, drop=False, inorder=True, projects=None, **kw):
@@ -47,5 +60,31 @@ class NewDocument(Document):
         await wait(tuple(map(mk_obj, rows)))
 
     @classmethod
-    def conv_data(cls, rows, header=None, mapper=None, **kw):
+    async def load_data(cls, rows, header=None, mapper=None, **kw):
         pass
+
+    @classmethod
+    async def load_file(cls, filename, encoding='auto', dupcheck=False, **kwargs):
+        kw = cls.loadfile_kw.copy()
+        kw.update(kwargs)
+        filename = Path(filename)
+        async with open(str(filename), 'rb')as f:
+            data = f.read()
+        if dupcheck and not LoadFile.check(cls.__name__, filename):
+            raise FileImported(filename.name)
+        if filename.lsuffix.startswith('.xls'):
+            book = xlrd.open_workbook(file_contents=data)
+            for index, sheet in enumerate(book.sheets()):
+                rows = sheet._cell_values
+                if rows:
+                    await cls.load_data(rows, sheetindex=index, sheet=sheet, **kw)
+        else:
+            if encoding == 'auto':
+                rows = decode(rows)
+            elif encoding == None:
+                pass
+            else:
+                rows = rows.decode(encoding)
+            await cls.load_data(rows, suffix=filename.suffix, **kw)
+        if dupcheck:
+            LoadFile.save(cls.__name__, filename)
