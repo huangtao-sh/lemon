b156e79d03b21823de2b4561cc5b649d96eb91ad
huangtao-sh <huangtao.jh@gmail.com>
2018-10-23 20:44:34 +0800

    upgrade

--- glemon/query.py
+++ glemon/query.py
@@ -227,7 +227,9 @@ class BaseQuery(object):
         return obj and self.document(from_query=True, **obj)
 
     def scalar(self, *fields):
-        # self.project(*fields)
+        from .loadfile import enlist
+        if len(fields) == 1:
+            fields = enlist(fields)
         if len(fields) == 1:
             def extract(d): return d.values(*fields)[0]
         else:
