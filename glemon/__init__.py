0fc50924fedbd24aaa33bd1a9aab633715916ba9
huangtao-sh <huangtao.jh@gmail.com>
2018-10-24 21:46:17 +0800

    upgrade

--- glemon/__init__.py
+++ glemon/__init__.py
@@ -3,11 +3,10 @@
 # 邮件：hunto@163.com
 # 创建：2017-07-22
 
-from .document import config, P, Document, FileImported, Descriptor
+from .document import config, P, Document, FileImported, Descriptor, enlist
 from .cachedquery import load_query
 from .__version__ import version as __version__
 from .shadow import Shadow, profile
-
 shadow = Shadow
 
 __all__ = 'config', 'P', 'Document', 'load_query', '__version__', 'Descriptor', 'shadow'
